f71fea6ac5d2ca830365ae684a97ea99e20b8481
omgSRT <thangckdt@gmail.com>
2024-09-22 13:10:13 +0700

    feat: dependency injection

--- ProductManagementAPI/Program.cs
+++ ProductManagementAPI/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.OData;
+using Repositories.Implement;
+using Repositories.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddOData(x => x.Select().Filter().OrderBy().Count().SetMaxTop(null).Expand());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
